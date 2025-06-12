163c5862ab8665eb73dfe7f2be3e9c7b2d64f87a
Gopmyc <hoyaux.gaspard@gmail.com>
2025-06-12 02:09:22 +0200

    refactor: 🧹 improve include_helpe structure

--- class.lua
+++ class.lua
@@ -1,19 +1,8 @@
 local function include_helper(to, from, seen)
-	if from == nil then
-		return to
-	elseif type(from) ~= 'table' then
-		return from
-	elseif seen[from] then
-		return seen[from]
-	end
+	if from == nil then return to elseif type(from) ~= 'table' then return from elseif seen[from] then return seen[from] end
 
 	seen[from] = to
-	for k,v in pairs(from) do
-		k = include_helper({}, k, seen)
-		if to[k] == nil then
-			to[k] = include_helper({}, v, seen)
-		end
-	end
+	for k,v in pairs(from) do k = include_helper({}, k, seen); if to[k] == nil then to[k] = include_helper({}, v, seen); end end
 	return to
 end
 
