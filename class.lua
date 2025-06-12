59e255f5629a9201e735a6aa0117ef9bd4086f4d
Gopmyc <hoyaux.gaspard@gmail.com>
2025-06-12 02:31:09 +0200

    feat: 🐞 add debugInfo method for object debugging and inspection

--- class.lua
+++ class.lua
@@ -10,6 +10,24 @@ return setmetatable(
 
         return tClass
     end,
+
+	debugInfo	=	function(self, tSelf)
+        local tMt	= 	(type(tSelf) == "userdata") and getmetatable(tSelf) or tSelf
+        local tStr	=	{
+            "==========================",
+            "=== DEBUG OBJECT INFO ===",
+            "==========================",
+            "\nType: " .. (tMt.__type or "Unknown"),
+            "\n# Private Data:",
+        }
+		
+        table.insert(tStr, (tMt.__private and table.concat({table.unpack(tMt.__private, function(k, v) return string.format("  - %s = %s", k, tostring(v)) end)}, "\n") or "  (empty)"))
+        table.insert(tStr, "\n# Methods:")
+        table.insert(tStr, table.concat((function() local methods = {} for sKey, vValue in pairs(tMt) do if type(vValue) == "function" then table.insert(methods, string.format("  - %s type : %s", sKey, type(vValue))) end end return methods end)(), "\n"))
+        table.insert(tStr, "\n==========================")
+		
+        return table.concat(tStr, "\n")
+    end,
 },
 {
     __call	=	function(self, ...)
