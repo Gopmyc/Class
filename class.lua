603bb2d3c84213fa53be48a5c92408bdd38d57fc
Gopmyc <hoyaux.gaspard@gmail.com>
2025-06-12 02:16:20 +0200

    style: 📝 apply naming convention to includeHelper function

--- class.lua
+++ class.lua
@@ -1,10 +1,13 @@
-local function include_helper(to, from, seen)
-	if from == nil then return to elseif type(from) ~= 'table' then return from elseif seen[from] then return seen[from] end
+local function includeHelper(tClass, tOther, tSeen)
+    if tOther == nil then return tClass end
+    if type(tOther) ~= 'table' then return tOther end
+    if tSeen[tOther] then return tSeen[tOther] end
+    
+    tSeen[tOther] = tClass
+    for k, v in pairs(tOther) do if not tSeen[k] then tClass[k] = includeHelper({}, v, tSeen) end end
 
-	seen[from] = to
-	for k,v in pairs(from) do k = include_helper({}, k, seen); if to[k] == nil then to[k] = include_helper({}, v, seen); end end
-	return to
-end
+    return tClass
+end  
 
 local function include(class, other) return include_helper(class, other, {}); end
 local function clone(other) return setmetatable(include({}, other), getmetatable(other)); end
