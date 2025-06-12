dd5a86be86d0dfccf67628be0e96c1524424c750
Gopmyc <hoyaux.gaspard@gmail.com>
2025-06-12 02:30:36 +0200

    feat: 🧩 add includeHelper method for deep table merging

--- class.lua
+++ class.lua
@@ -1,6 +1,17 @@
-return setmetatable({
-    
-}, {
+return setmetatable(
+{
+	includeHelper	=	function(self, tClass, tOther, tSeen)
+        if tOther == nil then           return tClass end
+        if type(tOther) ~= 'table' then return tOther end
+        if tSeen[tOther] then           return tSeen[tOther] end
+
+        tSeen[tOther]	=	tClass
+        for sKey, vValue in pairs(tOther) do if not tSeen[sKey] then tClass[sKey] = self:includeHelper({}, vValue, tSeen) end end
+
+        return tClass
+    end,
+},
+{
     __call	=	function(self, ...)
         return -- TODO : IMPLEMENT CLASS CREATION LOGIC HERE
     end
