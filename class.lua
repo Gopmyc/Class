2e0fd69e393b9b2a31bd3656a9a075a47c8330ed
Gopmyc <hoyaux.gaspard@gmail.com>
2025-06-12 02:03:42 +0200

    feat: 🧩 add include_helper function for deep table merging

---
+++ class.lua
@@ -0,0 +1,18 @@
+local function include_helper(to, from, seen)
+	if from == nil then
+		return to
+	elseif type(from) ~= 'table' then
+		return from
+	elseif seen[from] then
+		return seen[from]
+	end
+
+	seen[from] = to
+	for k,v in pairs(from) do
+		k = include_helper({}, k, seen)
+		if to[k] == nil then
+			to[k] = include_helper({}, v, seen)
+		end
+	end
+	return to
+end
